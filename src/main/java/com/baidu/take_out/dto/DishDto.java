3ca7d92236ae1e1e4d594a0a686068b655deea2a
haocibuni <1723541415@qq.com>
2023-07-24 23:37:02 +0800

    dish

---
+++ src/main/java/com/baidu/take_out/dto/DishDto.java
@@ -0,0 +1,18 @@
+package com.baidu.take_out.dto;
+
+import com.baidu.take_out.entity.Dish;
+import com.baidu.take_out.entity.DishFlavor;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class DishDto extends Dish {
+
+    //菜品对应的口味数据
+    private List<DishFlavor> flavors = new ArrayList<>();
+
+    private String categoryName;
+
+    private Integer copies;
+}
--- src/main/java/com/baidu/take_out/service/DishService.java
+++ src/main/java/com/baidu/take_out/service/DishService.java
@@ -1,5 +1,6 @@
 package com.baidu.take_out.service;
 
+import com.baidu.take_out.dto.DishDto;
 import com.baomidou.mybatisplus.extension.service.IService;
 //import com.baidu.take_out.dto.DishDto;
 import com.baidu.take_out.entity.Dish;
@@ -8,18 +9,18 @@ import java.util.List;
 
 public interface DishService extends IService<Dish> {
 
-//    //新增菜品，同时插入菜品对应的口味数据，需要操作两张表：dish、dish_flavor
-//    public void saveWithFlavor(DishDto dishDto);
-//
-//    //根据id查询菜品信息和对应的口味信息
-//    public DishDto getByIdWithFlavor(Long id);
-//
-//    //保存修改的菜品信息
-//    public void updateWithFlavor(DishDto dishDto);
-//
-//    //删除菜品信息
-//    public List<DishDto> deleteWithFlavor(List<Long> ids);
-//
-//    //改变菜品状态
-//    public void modifyStatus(Integer stu,List<Long> ids);
+    //新增菜品，同时插入菜品对应的口味数据，需要操作两张表：dish、dish_flavor
+    public void saveWithFlavor(DishDto dishDto);
+
+    //根据id查询菜品信息和对应的口味信息
+    public DishDto getByIdWithFlavor(Long id);
+
+    //保存修改的菜品信息
+    public void updateWithFlavor(DishDto dishDto);
+
+    //删除菜品信息
+    public List<DishDto> deleteWithFlavor(List<Long> ids);
+
+    //改变菜品状态
+    public void modifyStatus(Integer stu,List<Long> ids);
 }
